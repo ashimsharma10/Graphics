e3785583b1b8a8bb7e4c929365a7ec7c15ff6adf
Ashim Sharma <073bct508.ashim@pcampus.edu.np>
2019-05-27 21:10:28 +0545

    archive

---
+++ obj/build/CMakeFiles/ICTC.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ashim/semester_5/open/507_508_509/obj/CPP_files/Graphics.cc" "/home/ashim/semester_5/open/507_508_509/obj/build/CMakeFiles/ICTC.dir/CPP_files/Graphics.cc.o"
+  "/home/ashim/semester_5/open/507_508_509/obj/CPP_files/Math.cc" "/home/ashim/semester_5/open/507_508_509/obj/build/CMakeFiles/ICTC.dir/CPP_files/Math.cc.o"
+  "/home/ashim/semester_5/open/507_508_509/obj/CPP_files/Transformation.cc" "/home/ashim/semester_5/open/507_508_509/obj/build/CMakeFiles/ICTC.dir/CPP_files/Transformation.cc.o"
+  "/home/ashim/semester_5/open/507_508_509/obj/CPP_files/load.cc" "/home/ashim/semester_5/open/507_508_509/obj/build/CMakeFiles/ICTC.dir/CPP_files/load.cc.o"
+  "/home/ashim/semester_5/open/507_508_509/obj/main.cc" "/home/ashim/semester_5/open/507_508_509/obj/build/CMakeFiles/ICTC.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Native_files/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
